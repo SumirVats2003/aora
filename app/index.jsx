6292752c928a5e5288bdae62297c60b35171a4c6
Sumir Vats <sumirvats@gmail.com>
2024-07-28 10:58:02 +0530

    Added styled components, configs, constants and assets

--- app/index.jsx
+++ app/index.jsx
@@ -9,26 +9,32 @@
 // import { useEffect } from "react";
 
 // import { useColorScheme } from "@/hooks/useColorScheme";
+import React from "react";
+import { View, Text } from "react-native";
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
+import { styled } from "nativewind";
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
+const StyledLink = styled(Link);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <StyledView className='flex-1 items-center justify-center bg-white'>
+      <StyledText className='text-3xl font-pblack'>Aora!</StyledText>
       <StatusBar style='auto' />
-      <Link href='/profile' style={{ color: "blue" }}>
+      <StyledLink href='/profile' className='text-blue-500'>
         Go to Profile
-      </Link>
-    </View>
+      </StyledLink>
+    </StyledView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
